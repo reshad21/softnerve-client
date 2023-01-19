ca8d1f4c12b46526802624db434733f2c62c94e8
reshad21 <miakala7@gmail.com>
2023-01-19 13:19:01 +0600

    udate component create successfully

--- src/Pages/UpdateStudent/UpdateStudent.jsx
+++ src/Pages/UpdateStudent/UpdateStudent.jsx
@@ -1,9 +1,91 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateStudent = () => {
+    const { id } = useParams();
+    console.log(id);
+    const { data: student = [], isLoading } = useQuery({
+        queryKey: ['student', id],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/student/${id}`);
+            const data = await res.json();
+            return data;
+        }
+    })
+    if (isLoading) {
+        return (
+            <div className='bg-white flex items-end justify-center h-[200px]'>
+                <h1 className='text-2xl font-semibold text-slate-600'>Loading...</h1>
+            </div>
+        )
+    }
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        console.log('updated');
+    }
     return (
-        <div>
-            <h1>single ssudent update</h1>
+        <div className='mx-auto max-w-7xl'>
+            <h1>single ssudent update:{student?.sname}</h1>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content w-full flex flex-col">
+                    <div>
+                        <h1 className='text-5xl font-bold text-slate-700'>Update Student Information</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                        <form className="card-body" onSubmit={handleUpdate}>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Full Name</span>
+                                </label>
+                                <input type="text" name='sname' placeholder="name" className="input input-bordered" defaultValue={student?.sname} />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Father Name</span>
+                                </label>
+                                <input type="text" name='fname' placeholder="name" className="input input-bordered" defaultValue={student?.fname} />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Mother Name</span>
+                                </label>
+                                <input type="text" name='mname' placeholder="name" className="input input-bordered" defaultValue={student?.mname} />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Phone Number</span>
+                                </label>
+                                <input type="number" name='phoneNumber' placeholder="name" className="input input-bordered" defaultValue={student?.phoneNumber} />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Email Address</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email address" className="input input-bordered" defaultValue={student?.email} />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Permanent Address</span>
+                                </label>
+                                <input type="address" name='address' placeholder="Permanent address" className="input input-bordered" defaultValue={student?.address} />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Photo</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="Permanent address" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Update Information</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
